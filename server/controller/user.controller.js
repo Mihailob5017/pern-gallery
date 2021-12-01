4a9c3e72da5f8c7adb4589925e76497b4fb6b861
Mihailob5017 <mihailob5017@gmail.com>
2021-12-01 12:42:32 +0100

    added controllers for the user

---
+++ server/controller/user.controller.js
@@ -0,0 +1,11 @@
+// Login function
+export const login = async (req, res) => {};
+
+// Create account function
+export const createAccount = async (req, res) => {};
+
+// Get user profile function
+export const getProfile = async (req, res) => {};
+
+// Update user profile function
+export const updateProfile = async (req, res) => {};
