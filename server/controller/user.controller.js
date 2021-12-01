c2dedda5762170becc7a9889c03ecfef01e894f5
Mihailob5017 <mihailob5017@gmail.com>
2021-12-01 15:18:20 +0100

    basic functionality to create an account finished successfully

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,8 +1,32 @@
+import { query } from '../database/index.db.js';
+
 // Login function
 export const login = async (req, res) => {};
 
 // Create account function
-export const createAccount = async (req, res) => {};
+export const createAccount = async (req, res) => {
+	const { username, password, email, location, image_url, age } = req.body;
+
+	try {
+		// Add user to db and return him
+		const result = await query(
+			`
+            INSERT INTO users(username,age,location,email,password,image_url)
+            VALUES ($1,$2,$3,$4,$5,$6)
+            RETURNING *;
+            `,
+			[username, age, location, email, password, image_url]
+		);
+		res.status(200).json({ status: 'success', data: result.rows });
+	} catch (error) {
+		console.log(error.message);
+		res.status(error.statusCode).json({
+			clientMessage: 'Error creating account',
+			status: error.statusCode,
+			message: error.message,
+		});
+	}
+};
 
 // Get user profile function
 export const getProfile = async (req, res) => {};
--- server/database/index.db.js
+++ server/database/index.db.js
@@ -1,6 +1,7 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config('../.env');
+const { Pool } = pkg;
 
 const pool = new Pool();
 
